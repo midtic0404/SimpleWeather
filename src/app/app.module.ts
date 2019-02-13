2595f63f72116b6edcb7498b13d8a20d73de16ac
cchechang <cchechang@mastercontrol.com>
2019-02-12 23:19:57 -0700

    Added bootstrap 3 dependency and basic component structures

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { WeatherListComponent } from './weather-list/weather-list.component';
+import { WeatherItemComponent } from './weather-list/weather-item/weather-item.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    WeatherListComponent,
+    WeatherItemComponent
   ],
   imports: [
     BrowserModule
