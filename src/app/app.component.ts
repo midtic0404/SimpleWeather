71b2e494161cf664de482d49211417e6931274c2
Chia Che Chang <midtic0404@gmail.com>
2019-12-13 23:21:10 -0700

    Remove unnecessary code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,10 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  selectedMenu = 'list';
-
-  onNavigate(menu: string) {
-    this.selectedMenu = menu;
-  }
 
 }
